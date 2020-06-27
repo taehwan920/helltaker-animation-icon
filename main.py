03215fcf27c5a695fb95587cba4832fdedf0d5c6
Yang Taehwan <taehwan920@gmail.com>
2020-06-27 22:59:11 +0900

    All devils are ready to dance!

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import time
-from check_OS import your_OS
+from os import listdir
+# from os.path import isfile, join
 
 pygame.init()
 
@@ -14,36 +15,53 @@ SPRT_start_Y = 0
 SPRT_end_X = 150
 SPRT_end_Y = 150
 
+mydir = 'C:\\Users\\taehw\\Documents\\helltaker-animation-icon\\devils'
+devils = [f for f in listdir(mydir) if f[-4:] == '.png']
+
 pygame.display.set_caption('Cute Luciper!')
 background = pygame.image.load('background.png')
-lucifer = pygame.image.load('lucifer.png')
+sprite_idx = devils.index('Lucifer.png')
+sprite = pygame.image.load(f'{mydir}\\{devils[sprite_idx]}')
+# sprite = pygame.image.load(f'{mydir}\\Azazel.png')
 
 shut_down = False
 frame = True
 while not shut_down:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             shut_down = True
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_ESCAPE:
+                shut_down = True
+            if event.key == pygame.K_RIGHT:
+                sprite_idx = sprite_idx + 1 if sprite_idx + \
+                    1 <= len(devils) - 1 else 0
+                sprite = pygame.image.load(f'{mydir}\\{devils[sprite_idx]}')
+            if event.key == pygame.K_LEFT:
+                sprite_idx = sprite_idx - \
+                    1 if sprite_idx >= 1 else len(devils) - 1
+                sprite = pygame.image.load(f'{mydir}\\{devils[sprite_idx]}')
+
         if event.type == pygame.MOUSEBUTTONUP:
-            frame = not frame
             if frame:
                 screen = pygame.display.set_mode(
                     (SCREEN_X, SCREEN_Y), pygame.NOFRAME)
             else:
                 screen = pygame.display.set_mode((SCREEN_X, SCREEN_Y))
+            frame = not frame
 
-    lucifer.set_clip(pygame.Rect(
+    sprite.set_clip(pygame.Rect(
         SPRT_start_X, SPRT_start_Y, SPRT_end_X, SPRT_end_Y))
-    lucifer_pic = lucifer.subsurface(lucifer.get_clip())
+    sprite_cut = sprite.subsurface(sprite.get_clip())
 
     screen.blit(background, (0, 0))
-    screen.blit(lucifer_pic, (0, 0))
+    screen.blit(sprite_cut, (0, 0))
     pygame.display.update()
 
     SPRT_start_X = SPRT_start_X + 150 if SPRT_start_X + 150 <= 1650 else 0
     SPRT_end_X = SPRT_end_X + 150 if SPRT_end_X + 150 <= 1800 else 150
 
-    time.sleep(0.06)
+    time.sleep(0.055)
 
 pygame.quit()
